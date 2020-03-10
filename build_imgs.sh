2460b46318273ba6df543968ab384825ebe1b9d3
Yahya Oukharta <youkhart@e1r5p9.1337.ma>
2020-03-10 10:49:12 +0100

    update

--- build_imgs.sh
+++ build_imgs.sh
@@ -3,3 +3,6 @@ docker build wordpress/ -t wp-img
 docker build mysql/ -t mysql-img
 docker build grafana/ -t grafana-img
 docker build influxdb/ -t influxdb-img
+
+docker build mysql/init-db/ -t init-db-img
+docker build influxdb/init-influx/ -t init-influx-img
--- phpmyadmin/start.sh
+++ phpmyadmin/start.sh
@@ -1,12 +1,6 @@
 sh /setup_php.sh
 
-#create pma tables,
-while ! mysqladmin ping -h ${DB_HOST} --silent; do
-    echo "[!] Waiting for host : ${DB_HOST}"
-    sleep 1
-done
-
-mysql < /usr/share/webapps/phpmyadmin/sql/create_tables.sql mysql -h ${DB_HOST} -u admin -padmin 
+mysql < /usr/share/webapps/phpmyadmin/sql/create_tables.sql -h ${DB_HOST} -u admin -padmin 
 #start services
 rc-status
 rc-service nginx start
