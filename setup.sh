1cdb45bd3f87f1644b0956838c78aa883d69f1ac
Yahya Oukharta <youkhart@e2r3p8.1337.ma>
2020-10-15 04:53:17 +0100

    working on grafana dashboards

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ kubectl delete deployments --all
 kubectl delete pods --all
 kubectl delete services --all
 
-sh build_imgs.sh
+#sh build_imgs.sh
 
 kubectl apply -f deployments.yml
 kubectl apply -f services.yml
