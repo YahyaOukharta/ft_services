18423ffa4132af1f6f548e9a21645fdec4eb3a38
Yahya Oukharta <youkhart@e2r3p6.1337.ma>
2020-10-16 14:56:19 +0100

    pvc

--- setup.sh
+++ setup.sh
@@ -4,9 +4,15 @@
 kubectl delete deployments --all
 kubectl delete pods --all
 kubectl delete services --all
+kubectl delete pv --all
+kubectl delete pvc --all
 
 #sh build_imgs.sh
 minikube addons enable 'metallb'
 
+kubectl apply -f pvc.yml
+
 kubectl apply -f deployments.yml
 kubectl apply -f services.yml
+
+
