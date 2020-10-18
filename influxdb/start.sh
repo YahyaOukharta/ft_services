774c5b051fae12b5429029d7d80b813be95d39e6
Yahya Oukharta <youkhart@e2r3p8.1337.ma>
2020-10-18 02:15:32 +0100

    persistent volumes done

--- influxdb/start.sh
+++ influxdb/start.sh
@@ -2,7 +2,7 @@ rc-status
 #rc-service influxdb start
 #exec "/bin/sh"
 
-influxd &
+influxd -config=/etc/influxdb.conf &
 sleep 5
 influx -execute "CREATE USER telegraf WITH PASSWORD 'telegraf' WITH ALL PRIVILEGES"
 sleep 2
