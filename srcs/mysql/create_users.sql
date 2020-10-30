ca784bc04ab3941ea94c8d8795d43cf19f7a3098
Yahya Oukharta <youkhart@e2r2p6.1337.ma>
2020-10-31 00:52:02 +0100

    update

--- srcs/mysql/create_users.sql
+++ srcs/mysql/create_users.sql
@@ -5,23 +5,4 @@ GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' IDENTIFIED BY 'admin';
 GRANT ALL PRIVILEGES ON phpmyadmin.* TO 'pma'@'%' IDENTIFIED BY 'pmapass2';
 GRANT ALL PRIVILEGES ON wordpress.* TO 'wp_admin'@'%' IDENTIFIED BY 'wp_admin';
 
-/* Adding users to wordpress */
--- INSERT INTO `wp_users` (`ID`, `user_login`, `user_pass`, `user_nicename`, `user_email`, `user_url`, `user_registered`, `user_activation_key`, `user_status`, `display_name`) VALUES
---     (2, 'admin', '$P$BYXDvKuWHbCIS/kHDlFtRg.eeJ.wam/', 'admin', 'admin@users.cc', '', NOW(), '', 0, 'admin'),
---     (5, 'user1', '$P$Bd.pmNjUyz3Yrdm8ZmaCE6YvoRzi...', 'user1', 'user1@users.cc', '', NOW(), '', 0, 'user1'),
---     (6, 'user2', '$P$BxfFVy1dqx/nelIqsgACMmc0L8aR940', 'user2', 'user2@users.cc', '', NOW(), '', 0, 'user2');
-
--- INSERT INTO `wp_usermeta` (`user_id`, `meta_key`, `meta_value`) VALUES
---     (2, 'nickname', 'admin'),
---     (2, 'rich_editing', 'true'),
---     (2, 'syntax_highlighting', 'true'),
---     (2, 'comment_shortcuts', 'false'),
---     (2, 'admin_color', 'fresh'),
---     (2, 'show_admin_bar_front', 'true'),
---     (2, 'wp_capabilities', 'a:1:{s:13:\"administrator\";b:1;}'),
---     (2, 'wp_user_level', '10'),
---     (5, 'wp_capabilities', 'a:1:{s:6:\"editor\";b:1;}'),
---     (5, 'wp_user_level', '7'),
---     (6, 'wp_capabilities', 'a:1:{s:6:\"author\";b:1;}'),
---     (6, 'wp_user_level', '2');
 FLUSH PRIVILEGES;
--- srcs/mysql/start.sh
+++ srcs/mysql/start.sh
@@ -3,7 +3,7 @@ chmod -R 777 /var/lib/mysql
 export mysqldata=/var/lib/mysql/mysql
 if [ ! -d $mysqldata ]
 then
- /etc/init.d/mariadb setup
+ /etc/init.d/mariadb setup 
 fi
 
 rc-update add mariadb
@@ -13,8 +13,11 @@ rc-service mariadb restart
 rc-status
 
 mysql < create_users.sql -u root -proot
+
+if  mysql -u root -proot -e "select count(*) from information_schema.tables where table_schema='wordpress' and table_name='wp_users';" | grep "0";
+then
+    mysql < install_wp.sql -u root -proot
+fi
+
 rc-service mariadb stop
-
-
-
 mariadbd --user=root 
